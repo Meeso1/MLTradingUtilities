8f910d1d5776a2be195c195fdd6191fb12e45b22
Meeso1 <kubaknyspel@gmail.com>
2024-01-29 13:51:04 +0100

    `setup.py` contents

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name='MLTradingUtilities',
+      version='0.1',
+      description="Utilities for ML trading project",
+      author='Meeso1',
+      author_email='yeah',
+      url='https://github.com/Meeso1/MLTradingUtilities',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False)
